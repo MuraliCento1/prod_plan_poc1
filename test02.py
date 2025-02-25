48c550e092011c52e293a91eaf018df19ce0a15c
Sateesh1127 <satishkiran27@gmail.com>
2025-02-25 22:39:30 +0530

    changes to create table structures and update in db

--- test02.py
+++ test02.py
@@ -5,7 +5,27 @@ import logging
 
 logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 DATABASE_URL = "postgresql://postgres:799799@localhost:5432/postgres"
-
+#
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#
+# def create_tables():
+#     inspector = inspect(engine)
+#     tables = inspector.get_table_names()
+#     tables_list = ['vendor_inwarding_master', 'stock_master', 'vendor_master', 'production_plan_template', 'production_plan_template', 'sku_master']
+#     # Check and create VendorInwardingMaster table
+#     for table_name in tables_list:
+#         if table_name not in tables:
+#             VendorInwardingMaster.__table__.create(bind=engine)
+#             print("Table %s created.", table_name)
+#         else:
+#             print("Table %s already exists.", table_name)
+#
+#
+#
+# if __name__ == "__main__":
+#     create_tables()
+#
 engine = create_engine(DATABASE_URL)
 
 def create_tables():
